5294888004e3394cb1cfe8e2967d52ee4cd2ee77
Denis Luchkin-Zhou <denis@ricepo.com>
2015-11-17 01:35:31 -0500

    [CI Skip] Starting upgrade to Ignis 2.x

--- src/index.js
+++ src/index.js
@@ -1,102 +1,41 @@
 /**
- * auth/index.js
+ * index.js
  *
  * @author  Denis Luchkin-Zhou <denis@ricepo.com>
  * @license MIT
  */
 
-import _           from 'lodash';
-import Parser      from 'body-parser';
-import Passport    from 'passport';
-import * as JWT    from 'passport-jwt';
-import * as Local  from 'passport-local';
+const Ignis = require('parent-require')('ignis');
 
-import Strategy       from './strategy';
+
+/* Symbols to hide internal data */
+const $$strategies = Symbol();
+const $$aliases = Symbol();
+const $$options = Symbol();
 
 
 /**
- * passportFactory(2)
- *
- * @description                Authentication middleware factory.
- * @param          {ignis}     Ignis.js namespace.
- * @param          {handler}   Ignis.js request handler with metadata.
- * @returns        {Function}  Express.js middleware instance.
+ * PassportService class.
+ * Does not have an initialization callback.
  */
-export function passportFactory(ignis, handler) {
-  let options  = handler.auth || handler.authenticate || handler.authentication;
-  let strategy = options;
-  if (typeof options === 'object') {
-    strategy = options.strategy;
-    delete options.strategy;
-  } else {
-    options = { };
+@Ignis.Service.deps('http')
+export default class PassportService extends Ignis.Service {
+
+  constructor(ignis) {
+    super(ignis);
+
+    this[$$strategies] = new Map();
+    this[$$aliases] = new Map();
+    this[$$options] = { session: false };
   }
 
-  /* Generate nothing if strategy is 'none' or falsy */
-  if (!strategy || /^none$/i.test(strategy)) { return null; }
 
-  /* Resolve aliases first */
-  strategy = ignis.auth.__alias[strategy] || strategy;
-
-  /* Create the corresponding middleware */
-  options = _.merge({ }, ignis.auth.__options, options);
-  return function(req, res, next) {
-    let callback = passportCallback(req, res, next);
-    Passport.authenticate(strategy, options, callback)(req, res, next);
-  };
 }
 
 
 /**
- * passportCallback(3)
- *
- * @description                Creates a custom callback function so that
- *                             authentication errors go through Express.js
- *                             error handler stack instead of having Passport.js
- *                             send out a 401 response.
- * @param          {err}       Error occured during authentication.
- * @param          {user}      User information (if authentication successful).
- * @param          {info}      Additional information.
+ * Expose symbols
  */
-export function passportCallback(req, res, next) {
-  return function(err, user, info) {
-    if (err) { return next(err); }
-
-    if (!user) {
-      let error = new Error('Authentication Failed');
-      error.name = 'IgnisError';
-      error.status = 401;
-      error.sensitive = true;
-      error.details = {
-        sensitive: true,
-        reason: info
-      };
-      return next(error);
-    }
-
-    req.user = user;
-    next();
-  };
-}
-
-
-/**
- * auth(1)
- *
- * @description                Ignis.js extension.
- */
-export default function auth(ignis) {
-  /* Root authentication namespace */
-  ignis.auth = Object.create(null);
-  ignis.auth.__alias   = { 'token': 'jwt' };
-  ignis.auth.__options = { session: false };
-
-  /* Attach passport.js middlewares */
-  ignis.factories.push(passportFactory);
-  ignis.root.use(Parser.json());
-  ignis.root.use(Passport.initialize());
-
-  /* Authentication mechanisms */
-  ignis.auth.jwt   = Strategy(JWT.Strategy);
-  ignis.auth.local = Strategy(Local.Strategy);
-}
+PassportService.$$strategies = $$strategies;
+PassportService.$$aliases = $$aliases;
+PassportService.$$options = $$options;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,155 +1,21 @@
 /**
- * test/auth/index.spec.js
+ * test/index.spec.js
  *
- * @author  Denis-Luchkin-Zhou <denis@ricepo.com>
+ * @author  Denis Luchkin-Zhou <denis@ricepo.com>
  * @license MIT
  */
-/* jshint -W030 */
-var Sinon          = require('sinon');
-var Chai           = require('chai');
-var Bluebird       = require('bluebird');
-var Passport       = require('passport');
 
-Chai.use(require('chai-as-promised'));
+const Chai         = require('chai');
 Chai.use(require('sinon-chai'));
-var expect         = Chai.expect;
+Chai.use(require('chai-as-promised'));
 
-var Ignis          = require('ignis/lib/core');
-var extension      = require('../lib');
+/*!
+ * Setup global stuff here.
+ */
+global.co          = require('bluebird').coroutine;
+global.expect      = Chai.expect;
+global.Sinon       = require('sinon');
 
-
-describe('extension', function() {
-
-  before(function() {
-    Passport.__use = Passport.use;
-    Passport.__authenticate = Passport.authenticate;
-  });
-
-  after(function() {
-    Passport.use = Passport.__use;
-    Passport.authenticate = Passport.__authenticate;
-  });
-
-  beforeEach(function() {
-    Passport.use = Sinon.spy();
-    Passport.authenticate = Sinon.spy();
-
-    this.ignis = new Ignis();
-    this.ignis.root.use = Sinon.spy(this.ignis.root.use);
-  });
-
-  it('should mount modules to the specified namespace', function() {
-    this.ignis.use(extension);
-
-    return this.ignis.startup.then(() => {
-      expect(this.ignis.auth).to.be.an('object');
-      expect(this.ignis.auth.__alias).to.be.an('object');
-      expect(this.ignis.auth.__options).to.be.an('object');
-
-      expect(this.ignis.root.use).to.be.calledTwice;
-      expect(this.ignis.factories.length).to.equal(1);
-
-      expect(this.ignis.auth.jwt).to.be.a('function');
-      expect(this.ignis.auth.local).to.be.a('function');
-    });
-
-  });
-
-});
-
-describe('factory(2)', function() {
-
-  before(function() {
-    Passport.__use = Passport.use;
-    Passport.__authenticate = Passport.authenticate;
-  });
-
-  after(function() {
-    Passport.use = Passport.__use;
-    Passport.authenticate = Passport.__authenticate;
-  });
-
-  beforeEach(function() {
-    this.ignis = new Ignis();
-    this.ignis.use(extension);
-
-    Passport.use = Sinon.spy();
-    Passport.authenticate = Sinon.spy(function() {
-      return function() { };
-    });
-  });
-
-  it('should instantiate the appropriate middleware', function() {
-    var mw = extension.passportFactory(this.ignis, { auth: 'local' });
-    mw();
-
-    expect(Passport.authenticate).to.be.calledOnce;
-    expect(Passport.authenticate).to.be.calledWith('local');
-  });
-
-  it('should resolve aliases', function() {
-    var mw = extension.passportFactory(this.ignis, { auth: 'token' });
-    mw();
-
-    expect(Passport.authenticate).to.be.calledOnce
-    expect(Passport.authenticate).to.be.calledWith('jwt');
-  });
-
-  it('should handle options', function() {
-    var mw = extension.passportFactory(this.ignis, { auth: { strategy: 'token' } });
-    mw();
-
-    expect(Passport.authenticate).to.be.calledOnce;
-    expect(Passport.authenticate).to.be.calledWith('jwt');
-  });
-
-  it('should return null when no strategy is specified', function() {
-    var mw = extension.passportFactory(this.ignis, { });
-    expect(mw).to.equal(null);
-  });
-
-  it('should return null when strategy is \'none\'', function() {
-    var mw = extension.passportFactory(this.ignis, { auth: 'noNe' });
-    expect(mw).to.equal(null);
-  });
-
-});
-
-describe('callback(3)', function() {
-
-  beforeEach(function() { this.callback = Sinon.spy(); });
-
-  it('should return a callback function', function() {
-    var cb = extension.passportCallback(null, null, this.callback);
-    expect(cb).to.be.a('function');
-  });
-
-  it('should not generate errors when successful', function() {
-    var cb = extension.passportCallback(Object.create(null), null, this.callback);
-
-    cb(null, Object.create(null), null);
-    expect(this.callback.calledOnce).to.equal(true);
-    expect(this.callback.calledWithExactly()).to.equal(true);
-  });
-
-  it('should correctly pass on errors', function() {
-    var cb = extension.passportCallback(null, null, this.callback);
-
-    cb('foobar', Object.create(null), null);
-    expect(this.callback.calledOnce).to.equal(true);
-    expect(this.callback.calledWith('foobar')).to.equal(true);
-  });
-
-  it('should correctly generate Authentication Failed errors', function() {
-    var cb = extension.passportCallback(null, null, this.callback);
-
-    cb(null, null, 'foobar');
-    expect(this.callback.calledOnce).to.equal(true);
-    expect(this.callback.args[0][0])
-      .to.be.an.instanceOf(Error).and
-      .have.deep.property('details.reason', 'foobar');
-  });
-
-});
-
-require('./strategy.spec.js');
+/*!
+ * Start tests.
+ */
