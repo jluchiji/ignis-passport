bdee1622460186a553560927744db9e32250dfb8
Denis Luchkin-Zhou <denis@ricepo.com>
2015-11-17 04:21:12 -0500

    Updated to work with ignis ^2.0.0

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@
  * @author  Denis Luchkin-Zhou <denis@ricepo.com>
  * @license MIT
  */
-
-const Ignis = require('parent-require')('ignis');
+import _           from 'lodash';
+import Ignis       from 'ignis';
+import Parser      from 'body-parser';
+import Passport    from 'passport';
+import * as JWT    from 'passport-jwt';
+import * as Local  from 'passport-local';
+import {
+  autobind
+}                  from 'core-decorators';
 
 
 /* Symbols to hide internal data */
-const $$strategies = Symbol();
 const $$aliases = Symbol();
 const $$options = Symbol();
 
@@ -24,11 +30,113 @@ export default class PassportService extends Ignis.Service {
   constructor(ignis) {
     super(ignis);
 
-    this[$$strategies] = new Map();
-    this[$$aliases] = new Map();
+    this[$$aliases] = { token: 'jwt' };
     this[$$options] = { session: false };
   }
 
+
+  /*!
+   * Initialization callback.
+   */
+  async init(http) {
+
+    /* Push required middleware into the router */
+    http.router.use(Parser.json());
+    http.router.use(Passport.initialize());
+
+    /* Push a middleware factory before the callback */
+    http.pre(this.factory);
+
+    /* Attach default authentication strategies */
+    this.strategy(JWT.Strategy);
+    this.strategy(Local.Strategy);
+
+  }
+
+
+  /*!
+   * Defines an authentication strategy.
+   * Returns a wrapper function that unpsomisifies the callback.
+   */
+  strategy(type) {
+    return function(callback) {
+      let options  = Object.create(null);
+      if (arguments.length === 2) {
+        options  = arguments[0];
+        callback = arguments[1];
+      }
+
+      const result = new type(options, Ignis.Util.unpromisify(callback));
+      Passport.use(result);
+    };
+  }
+
+
+  /*!
+   * Defines an alias for a strategy.
+   */
+  @autobind
+  alias(strategy, aka) {
+    this[$$aliases][aka] = strategy;
+  }
+
+
+  /*!
+   * Creates a passport.js callback.
+   */
+  callback(req, res, next) {
+    return function(err, user, info) {
+      if (err) { return next(err); }
+
+      if (!user) {
+        const error = new Ignis.Error('Authentication Failed');
+        error.status = 401;
+        error.sensitive = true;
+        error.details = {
+          sensitive: true,
+          reason: info
+        };
+        return next(error);
+      }
+
+      req.user = user;
+      next();
+    };
+  }
+
+
+  /*!
+   * Middleware factory function.
+   */
+  @autobind
+  factory(ignis, handler) {
+    let options  = handler.auth;
+    let strategy = options;
+
+    /* Allow additional options */
+    if (typeof options === 'object') {
+      strategy = options.strategy;
+      delete options.strategy;
+    } else {
+      options = { };
+    }
+
+    /* Generate nothing if strategy is 'none' or falsy */
+    if (!strategy || /^none$/i.test(strategy)) { return null; }
+
+    /* Resolve aliases first */
+    const aliases = this[$$aliases];
+    while (aliases[strategy]) {
+      strategy = aliases[strategy];
+    }
+
+    /* Create the corresponding middleware */
+    options = _.defaults(options, this[$$options]);
+    return (req, res, next) => {
+      const callback = this.callback(req, res, next);
+      Passport.authenticate(strategy, options, callback)(req, res, next);
+    };
+  }
 
 }
 
@@ -36,6 +144,5 @@ export default class PassportService extends Ignis.Service {
 /**
  * Expose symbols
  */
-PassportService.$$strategies = $$strategies;
 PassportService.$$aliases = $$aliases;
 PassportService.$$options = $$options;
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,11 @@
  * @license MIT
  */
 
-const Chai         = require('chai');
+const Chai = require('chai');
+const Ignis = require('ignis');
+const Passport = require('passport');
+const PassportService = require('../lib');
+
 Chai.use(require('sinon-chai'));
 Chai.use(require('chai-as-promised'));
 
@@ -17,5 +21,189 @@ global.expect      = Chai.expect;
 global.Sinon       = require('sinon');
 
 /*!
- * Start tests.
+ * Setup/Teardown
  */
+before(function() {
+  Passport.__use = Passport.use;
+  Passport.__authenticate = Passport.authenticate;
+});
+
+after(function() {
+  Passport.use = Passport.__use;
+  Passport.authenticate = Passport.__authenticate;
+});
+
+beforeEach(co(function*() {
+  Passport.use = Sinon.spy();
+  Passport.authenticate = Sinon.spy(() => function() { });
+
+  Ignis.reset();
+  this.ignis = Ignis();
+
+  this.ignis.use(PassportService);
+  yield this.ignis.init();
+
+  this.http = this.ignis.service('http');
+  this.http.router.use = Sinon.spy(this.http.router.use);
+  this.service = this.ignis.service('passport');
+}));
+
+describe('init(http)', function() {
+
+  it('should push auth middleware factory', function() {
+    const factories = this.http[Ignis.Http.$$pre];
+    expect(factories)
+      .to.have.length(1);
+    expect(factories[0])
+      .to.equal(this.service.factory);
+  });
+
+});
+
+describe('alias(strategy, aka)', function() {
+
+  it('should define an alias', function() {
+    this.service.alias('foo', 'bar');
+    const aliases = this.service[PassportService.$$aliases];
+    expect(aliases)
+      .to.have.property('bar', 'foo');
+  });
+
+});
+
+describe('factory(ignis, handler)', function() {
+
+  it('should instantiate the appropriate middleware', function() {
+    const mw = this.service.factory(this.ignis, { auth: 'local' });
+    mw();
+
+    expect(Passport.authenticate)
+      .to.be.calledOnce
+      .to.be.calledWith('local');
+  });
+
+  it('should resolve aliases', function() {
+    const mw = this.service.factory(this.ignis, { auth: 'token' });
+    mw();
+
+    expect(Passport.authenticate)
+      .to.be.calledOnce
+      .to.be.calledWith('jwt');
+  });
+
+  it('should handle on options', function() {
+    const mw = this.service.factory(this.ignis, { auth: { strategy: 'token' } });
+    mw();
+
+    expect(Passport.authenticate)
+      .to.be.calledOnce
+      .to.be.calledWith('jwt');
+  });
+
+  it('should return null when no strategy is specified', function() {
+    const mw = this.service.factory(this.ignis, { });
+    expect(mw)
+      .to.equal(null);
+  });
+
+  it('should return null when strategy is \'none\'', function() {
+    const mw = this.service.factory(this.ignis, { auth: 'noNe' });
+    expect(mw)
+      .to.equal(null);
+  });
+
+});
+
+describe('callback(req, res, next)', function() {
+
+  beforeEach(function() {
+    this.cb = Sinon.spy();
+  });
+
+  it('should return a callback function', function() {
+    const cb = this.service.callback(null, null, this.cb);
+    expect(cb)
+      .to.be.a('function');
+  });
+
+  it('should not generate errors when successful', function() {
+    const cb = this.service.callback({ }, null, this.cb);
+    cb(null, { }, null);
+
+    expect(this.cb)
+      .to.be.calledOnce;
+    expect(this.cb.firstCall.args)
+      .to.have.length(0);
+  });
+
+  it('should pass on errors if fails', function() {
+    const cb = this.service.callback({ }, null, this.cb);
+    cb('foobar', { }, null);
+
+    expect(this.cb)
+      .to.be.calledOnce
+      .to.be.calledWith('foobar');
+  });
+
+  it('should generate Ignis.Error instances', function() {
+    const cb = this.service.callback(null, null, this.cb);
+
+    cb(null, null, 'foobar');
+
+    expect(this.cb)
+      .to.be.calledOnce;
+    const actual = this.cb.firstCall.args[0];
+    expect(actual)
+      .to.exist
+      .to.be.an.instanceOf(Ignis.Error)
+      .to.have.deep.property('details.reason', 'foobar');
+
+  });
+
+});
+
+describe('strategy(type)', function() {
+
+  beforeEach(function() {
+    const spy = this.spy = Sinon.spy();
+    this.type = function(options, callback) {
+      this.options = options;
+      this.callback = callback;
+      spy.apply(null, arguments);
+    };
+  });
+
+  it('should produce a strategy factory', function() {
+    const factory = this.service.strategy(this.type);
+    expect(factory)
+      .to.be.a('function');
+
+    const options = { foo: 'bar' };
+    const callback = Sinon.spy();
+
+    factory(options, callback);
+
+    expect(this.spy)
+      .to.be.calledOnce
+      .to.be.calledWith(options);
+    expect(Passport.use)
+      .to.be.calledOnce;
+  });
+
+  it('should handle absent options parameter', function() {
+    const factory = this.service.strategy(this.type);
+    expect(factory)
+      .to.be.a('function');
+
+    const callback = Sinon.spy();
+
+    factory(callback);
+
+    expect(this.spy)
+      .to.be.calledOnce
+      .to.be.calledWith({ });
+    expect(Passport.use)
+      .to.be.calledOnce;
+  });
+
+});
